170f51e3fd058dbe3f256a0ace0332d603b85cad
Wesley Vollmar <wcvollmar@marathonpetroleum.com>
2018-03-22 17:12:48 -0400

    - Removed limitation on the number of input numbers - Implemented tests for 5 and 8 inputs

--- StringCalculator.Tests/StringCalculatorShould.cs
+++ StringCalculator.Tests/StringCalculatorShould.cs
@@ -43,5 +43,31 @@ namespace StringCalculator.Tests
             // Assert
             Assert.AreEqual(3, result);
         }
+
+        [TestMethod]
+        public void ReturnSumGivenFiveNumbers()
+        {
+            // Arrange
+            var input = "1,2,6,12,3";
+
+            // Act
+            var result = StringCalculator.Add(input);
+
+            // Assert
+            Assert.AreEqual(24, result);
+        }
+
+        [TestMethod]
+        public void ReturnSumGivenEightNumbers()
+        {
+            // Arrange
+            var input = "4,1,8,2,7,3,0,2";
+
+            // Act
+            var result = StringCalculator.Add(input);
+
+            // Assert
+            Assert.AreEqual(27, result);
+        }
     }
 }
--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -11,7 +11,7 @@ namespace StringCalculator
         /// <summary>
         /// Method to add numbers, as encoded in string input
         /// </summary>
-        /// <param name="numbers">String of 0 to 2 numbers to add, separated by comma</param>
+        /// <param name="numbers">String of numbers to add, separated by comma</param>
         /// <returns>Integer of the additive result of the input numbers</returns>
         public static int Add(string numbers)
         {
@@ -19,9 +19,6 @@ namespace StringCalculator
             var parsedInput = (numbers ?? string.Empty)
                 .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
                 .Select(i => int.Parse(i));
-
-            if (parsedInput.Count() > 2)
-                throw new ArgumentException("Only 0, 1, or 2 numbers are accepted, each separated by ','", nameof(numbers));
 
             // Calculate results
             var result = 0;
